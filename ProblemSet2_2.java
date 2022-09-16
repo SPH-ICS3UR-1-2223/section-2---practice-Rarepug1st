3c246bf5ce704ae7cc2d80ae10ac731955fbf24c
Rarepug1st <99208797+Rarepug1st@users.noreply.github.com>
2022-09-16 14:24:03 -0400

    imd

--- ProblemSet2_2.java
+++ ProblemSet2_2.java
@@ -2,7 +2,8 @@ public class ProblemSet2_2 {
     public static void main(String[] args) {
         //Use print statements to create your own beautiful original ASCII art.
         //Use comments to describe what your image is depicting.
-        
+        System.out.println("___________");
+        System.out.println("    V V    ");
         
         
         
